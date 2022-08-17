cac4eb10b9bda51db8372ea3cdc3372422fd7796
Paul <p22626262@gmail.com>
2022-08-17 15:12:13 +0800

    fix: remove verify cmd from telegram bot

--- bot/telegram/cmd_help.go
+++ bot/telegram/cmd_help.go
@@ -10,18 +10,17 @@ import (
 // HandleHelp handles the the request for help by the user
 func (bot *Bot) HandleHelp(ctx telebot.Context) error {
 	// Answer to the command
+	//TODO: implement verify command
 	ctx.Reply(fmt.Sprintf(
 		"Here are the available commands:\n"+
 			"- `/%s`, to get help\n"+
 			"- `/%s`, to read the documentation\n"+
 			"- `/%s <address>`, to ask for testnet tokens\n"+
-			"- `/%s`, to connect your Desmos profile to Telegram\n"+
-			"- `/%s`, to verify the connection between Telegram and your Desmos profile\n",
+			"- `/%s`, to connect your Desmos profile to Telegram\n",
 		types.CmdHelp,
 		types.CmdDocs,
 		types.CmdSend,
 		types.CmdConnect,
-		types.CmdVerify,
 	))
 
 	return nil
