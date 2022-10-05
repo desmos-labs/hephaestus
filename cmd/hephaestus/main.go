1705e3561c8c56c6ed21c95170432ca4e77764c6
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-10-05 15:28:21 -0500

    fix: properly setup Cosmos SDK config to parse addresses

--- cmd/hephaestus/main.go
+++ cmd/hephaestus/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"os"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/desmos-labs/desmos/v4/app"
+
 	"github.com/desmos-labs/hephaestus/cmd"
 )
 
 func main() {
+	// Setup the Cosmos SDK config
+	app.SetupConfig(sdk.GetConfig())
+
 	executor := cmd.RootCmd()
 	if err := executor.Execute(); err != nil {
 		os.Exit(1)
